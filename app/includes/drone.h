7c3fd11bdf67b24a3d88ca18cf377d8a1ae1b275
NeverBackPing <neverbackping@gmail.com>
2025-04-27 16:42:57 +0200

    split file edit CMAKE

---
+++ app/includes/drone.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                              NEVERBACKPING                                 */
+/*                          Embedded RTOS Drone Project                       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	DRONE_H
+# define DRONE_H
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/rtc.h>
+#include <zephyr/sys/util.h>
+#include "mpu6050.h"
+
+
+# endif
